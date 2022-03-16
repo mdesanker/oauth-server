310a18819d8aa9c50923ce39cd856ab0681c857d
Michael Desanker <mdesanker@gmail.com>
2022-03-16 05:13:45 -0400

    Add user model

---
+++ models/User.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import { DateTime } from "luxon";
+
+export interface IUser {
+  _id?: string;
+  googleId?: string;
+  username: string;
+  email: string;
+  date: DateTime;
+}
+
+const UserSchema = new Schema<IUser>({
+  googleId: { type: String },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  date: { type: Date, default: DateTime.now },
+});
+
+export default model("User", UserSchema);
