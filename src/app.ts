5aa858c4a00dfb97e96593ae64964a96377ed032
Michael Desanker <mdesanker@gmail.com>
2022-03-16 05:04:06 -0400

    Add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [/\localhost/],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Oauth2.0 Server");
+});
 
 const PORT = (process.env.PORT as string) || (process.env.DEV_PORT as string);
 
