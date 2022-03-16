0daa7f414bd461344482e559145b546c87524dad
Michael Desanker <mdesanker@gmail.com>
2022-03-16 05:38:29 -0400

    Setup passport middleware

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
-import connectDB from "./config.ts/mongoConfig";
+import cookieSession from "cookie-session";
+import passport from "passport";
 
+import connectDB from "./config.ts/mongoConfig";
 import authRouter from "./routes/api/auth";
+import "./strategies/googleStrategy";
 
 const app = express();
 
@@ -21,6 +24,16 @@ app.use(helmet());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.SESSION_KEY as string],
+    maxAge: 1000 * 60 * 60 * 6,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Oauth2.0 Server");
